3d9ac0a904193853e5d3a1cc1a436d3e4d00d2bc
Alexander Guettler <xforce@crix-dev.org>
2016-06-20 17:40:14 +0200

    Revert project files, didn't mean to commit them

--- JustFOV/Properties/AssemblyInfo.cs
+++ JustFOV/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Just Cause 3 FOV Patcher")]
-[assembly: AssemblyCopyright("Copyright © Trix 2015, xforce 2016")]
+[assembly: AssemblyCopyright("Copyright © Trix 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
